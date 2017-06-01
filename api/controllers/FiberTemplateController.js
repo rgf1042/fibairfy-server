c73fb7385e308204047c8214772d0e2e3e77acc8
Roger Garcia <roger.garcia@guifi.net>
2017-06-01 13:08:43 +0200

    FiberTemplate API done

--- api/controllers/FiberTemplateController.js
+++ api/controllers/FiberTemplateController.js
@@ -11,15 +11,129 @@ module.exports = class FiberTemplateController extends Controller{
     return ('FiberTemplate')
   }
   create(request, reply) {
-    this.app.services.RestfulService.create(this._Model(), request, reply)
+    this.log.debug('[',this.constructor.name,'] (create) model =',
+      this._Model(), ', criteria =', request.query,
+      ', values = ', request.body)
+
+    request.body.user = request.user.id
+
+    let that = this
+
+    if (!request.body
+      || !request.body.user) {
+      reply.json({flag: false, data: '', message: 'Error!'})
+      return
+    }
+
+    const FootprintService = that.app.services.FootprintService
+
+    FootprintService.create(this._Model(), request.body)
+      .then(elements => {
+        reply.status(200).json(elements || {})
+      }).catch(error => {
+        if (error.code == 'E_VALIDATION') {
+          reply.status(400).json(error)
+        }
+        else if (error.code == 'E_NOT_FOUND') {
+          reply.status(404).json(error)
+        }
+      else {
+          reply.boom.wrap(error)
+        }
+      })
   }
   find(request, reply) {
-    this.app.services.RestfulService.find(this._Model(), request, reply)
+    const FootprintService = this.app.services.FootprintService
+
+    const id = request.params.id
+
+    this.log.debug('[',this.constructor.name,'] (find) model =',
+      this._Model(), ', criteria =', request.query, id,
+      ', values = ')
+
+    let response
+
+    //let where =  { user: request.user.id }
+    let where = {}
+    if (id) where.id = id
+
+    response = FootprintService.find(this._Model(), where)
+
+    response.then(elements => {
+      reply.status(elements ? 200 : 404).json(elements || {})
+    }).catch(error => {
+      if (error.code == 'E_VALIDATION') {
+        reply.status(400).json(error)
+      }
+      else if (error.code == 'E_NOT_FOUND') {
+        reply.status(404).json(error)
+      }
+      else {
+        reply.boom.wrap(error)
+      }
+    })
   }
   update(request, reply) {
-    this.app.services.RestfulService.update(this._Model(), request, reply)
+    const id = request.params.id
+    this.log.debug('[',this.constructor.name,'] (update) model =',
+      this._Model(), ', criteria =', request.query, id,
+      ', values = ', request.body)
+    let response
+
+    let that = this
+    let where =  { user: request.user.id }
+    if (id) where.id = id
+
+    const FootprintService = that.app.services.FootprintService
+    response = FootprintService.update(this._Model(), where , request.body)
+
+    response.then(elements => {
+      reply.status(200).json(elements || {})
+    }).catch(error => {
+      if (error.code == 'E_VALIDATION') {
+        reply.status(400).json(error)
+      }
+      else if (error.code == 'E_NOT_FOUND') {
+        reply.status(404).json(error)
+      }
+      else {
+        reply.boom.wrap(error)
+      }
+    })
   }
   destroy(request, reply) {
-    this.app.services.RestfulService.destroy(this._Model(), request, reply)
+    const id = request.params.id
+    this.log.debug('[',this.constructor.name,'] (destroy) model =',
+      this._Model(), ', criteria =', request.query, id, request.user.username, request.user.id)
+
+    let response
+
+    let that = this
+
+    const FootprintService = this.app.services.FootprintService
+    let where =  { user: request.user.id }
+    if (!id) {
+      reply.status(500).json({flag: false, data: '', message: 'Error, you can not remove without id.'})
+      return
+    }
+
+    where.id = id
+
+    response = FootprintService.destroy(this._Model(), where)
+
+    response.then(elements => {
+      reply.status(200).json(elements || {})
+    }).catch(error => {
+      if (error.code == 'E_VALIDATION') {
+        reply.status(400).json(error)
+      }
+      else if (error.code == 'E_NOT_FOUND') {
+        reply.status(404).json(error)
+      }
+      else {
+        reply.boom.wrap(error)
+      }
+    })
   }
+
 }
--- api/models/FiberTemplate.js
+++ api/models/FiberTemplate.js
@@ -18,6 +18,9 @@ module.exports = class FiberTemplate extends Model {
       },
       colors: {
         type: 'string'
+      },
+      user: {
+        model: 'User'
       }
     }
   }
