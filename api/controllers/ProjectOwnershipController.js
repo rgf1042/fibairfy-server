d6b3794fb0fb8bb2a136f4807874043394ef4ebd
Roger Garcia <roger.garcia@guifi.net>
2017-12-17 13:08:42 +0100

    New improvements: 	- ProjectOwnershipController created 	- ProjectOwnershipController ready to add owners per project using Blueprint API

---
+++ api/controllers/ProjectOwnershipController.js
@@ -0,0 +1,10 @@
+/**
+ * ProjectOwnershipController
+ *
+ * @description :: Server-side logic for managing Project owner permissions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
