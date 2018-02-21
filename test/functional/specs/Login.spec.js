9e3ad097429ba1f8b878e6120c306b62879a3a2a
agusti <agusti@gmail.com>
2018-02-21 16:39:18 +0100

    [TDD]: Create Login spec

--- test/functional/specs/Login.spec.js
+++ test/functional/specs/Login.spec.js
@@ -2,22 +2,66 @@ const request = require('supertest')
 const chai = require('chai')
 const faker = require('faker')
 
-describe('Login', function() {
-  										it('should local login', function(done) {
-    										let name = 'agusti'
-    										let password = '1234'
+const name = "test"
+const passwd = "password"
+const wrong_passwd = "wrong-password"
 
-    										request(sails.hooks.http.app)
-      .post('/auth/login')
-      .send({
-        										name,
-        										password
-      })
-      .expect(200)
-      .then(() => {
-          										chai.assert.isNotEmpty(response.body.token)
-          										done()
-      })
+describe('Login', function() {
+  it('should create local User', function(done){
+    // Create User
+    User
+    .create({username:name,password:passwd}).exec(function(err, user) {
+      chai.assert.equal(user.username, name)
+      done()
+    })
+  })
+
+  it('should local login', function(done) {
+    request(sails.hooks.http.app)
+    .post('/auth/login')
+    .send({ username:name, password:passwd })
+    .end(function(err, response) {
+      if (err) return done(err)
+
+      chai.assert.isNotEmpty(response.body.token)
+      done()
+    })
+  })
+
+  it('should local login with wrong password', function(done) {
+    request(sails.hooks.http.app)
+    .post('/auth/login')
+    .send({ username:name, password:wrong_passwd })
+    .end(function(err, response) {
+      if (err) return done(err)
+
+      chai.assert.equal(response.body.flag, false)
+      done()
+    })
+  })
+
+  it('should local login with empty parameters', function(done) {
+    request(sails.hooks.http.app)
+    .post('/auth/login')
+    .send()
+    .end(function(err, response) {
+      if (err) return done(err)
+
+      chai.assert.equal(response.body.flag, false)
+      done()
+    })
+  })
+
+  it('should local login with wrong username', function(done) {
+    request(sails.hooks.http.app)
+    .post('/auth/login')
+    .send({ username:wrong_passwd, password:passwd })
+    .end(function(err, response) {
+      if (err) return done(err)
+
+      chai.assert.equal(response.body.flag, false)
+      done()
+    })
   })
 
 })
