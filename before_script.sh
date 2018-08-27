cda2cdb0fd7bfc50674f6d2d3a802f34adebcc75
Roger Garcia <roger.garcia@guifi.net>
2018-08-27 20:46:46 +0200

    New travis script modification

--- before_script.sh
+++ before_script.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 touch INSTALLED
 docker pull guifi/fiberfy:sails
 docker run guifi/fiberfy:sails -v "$PWD":/usr/share/node/fiberfy /bin/sh -c "gosu fiberfy npm run build && gosu fiberfy npm run test-dev"
