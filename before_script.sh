ed899945e5077b3d61ad5fb80fc0285e3217fb30
Roger Garcia <roger.garcia@guifi.net>
2018-08-27 21:58:53 +0200

    New script modification

--- before_script.sh
+++ before_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch INSTALLED
 docker pull guifi/fiberfy:sails
-docker run -v $(pwd):/usr/share/node/fiberfy guifi/fiberfy:sails /bin/sh -c "gosu fiberfy npm run build && gosu fiberfy npm run test-dev"
+docker run -v $(pwd):/usr/share/node/fiberfy guifi/fiberfy:sails gosu fiberfy bash -c 'npm run build && gosu fiberfy npm run test-dev'
