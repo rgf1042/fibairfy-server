0d7da807a2623181a594cc87daa35021e0df2a4f
Roger Garcia <rgf1042@gmail.com>
2019-05-18 20:08:18 +0200

    Continuous Delivery configured for Docker images

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=rgf1042/fibairfy-server
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+docker build -f Dockerfile -t $REPO:$TRAVIS_TAG
 docker push $REPO
