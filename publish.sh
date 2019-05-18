223d4d82eb23aefe4dd265ce4b424b216e99fb97
Roger Garcia <rgf1042@gmail.com>
2019-05-18 20:24:10 +0200

    Travis delivery script modified.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker login -u $DOCKER_USER -p $DOCKER_PASS
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 export REPO=rgf1042/fibairfy-server
-docker build -f Dockerfile -t $REPO:$TRAVIS_TAG
+docker build -t $REPO:$TRAVIS_TAG .
 docker push $REPO
